d5f3a898d8a35de49395ac95361521020d1947f9
Truc Phan <pttruc.308@gmail.com>
2021-08-29 19:50:54 -0700

    add product and review modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,32 @@
 import React from 'react';
-import data from './data';
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div class="grid-container">
-    <header className="row">
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="row">
         <div>
-            <a class="brand" href="/">DEHYPE</a>
+          <a className="brand" href="/">
+            DEHYPE 
+          </a>
         </div>
         <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
-    </header>
-    <main>
-        <div>
-        <div class="row center">
-          {
-            data.products.map(product => (
-              <div key={product._id} class="card">
-                <a href={`/product/${product._id}`}>
-                    <img class="medium" 
-                    src={product.image} 
-                    alt={product.name}/>
-                </a>
-                <div class="card-body">
-                    <a href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2>
-                    </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                    </div>
-                    <div class="price">${product.price}</div>
-                </div>
-            </div>
-            ))
-          }
-        </div>
-        </div>
-      </main>
-    <footer class="row center">All rights reserved</footer>
-  </div>
-  );
-}
+      </header>
+      <main>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path ="/" component={HomeScreen} exact></Route>
+        
+        </main>
+        <footer className="row center">All rights reserved</footer>
+      </div>
+    </BrowserRouter>
+    );
+  }
 
 export default App;
