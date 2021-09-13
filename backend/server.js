42367da2ec0bc92190c7efc5c3c2aa7b1a37855f
Truc Phan <pttruc.308@gmail.com>
2021-09-12 23:59:38 -0700

    implement sign in and register functionalities

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,24 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import data from './data.js';
-import userRouter from './routers/userRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+
+dotenv.config();
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/dehype', {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/dehype", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
-app.use('/api/users', userRouter);
-app.get('/', (req, res) => {
-  res.send('Server is ready');
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
 app.use((err, req, res, next) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,67 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import {BrowserRouter, Link, Route} from 'react-router-dom';
-import CartScreen from './screens/CartScreen';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import { signout } from "./actions/userActions";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
-  const cart = useSelector(state => state.cart);
-  const {cartItems} = cart;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <Link className="brand" to="/">
-            DEHYPE 
-          </Link>
-        </div>
-        <div>
-          <Link to="/cart">Cart
-          {
-            cartItems.length > 0 && (
-              <span className="badge">{cartItems.length}</span>
-            )
-          }
-          </Link>
-          <Link to="/signin">Sign In</Link>
-        </div>
-      </header>
-      <main>
-        <Route path="/cart/:id?" component={CartScreen}></Route>
-        <Route path="/product/:id" component={ProductScreen}></Route>
-        <Route path ="/" component={HomeScreen} exact></Route>
-        
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              DEHYPE
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="#signout" onClick={signoutHandler}>
+                    Sign Out
+                  </Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved</footer>
       </div>
     </BrowserRouter>
-    );
-  }
+  );
+}
 
 export default App;
