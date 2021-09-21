fbe972a7ffa51b0e36f845779b86b7aa8cd6e5b4
Truc Phan <pttruc.308@gmail.com>
2021-09-20 22:18:10 -0700

    implement update user info

--- backend/utils.js
+++ backend/utils.js
@@ -35,3 +35,11 @@ export const isAuth = (req, res, next) => {
     res.status(401).end({ message: "Invalid Token" });
   }
 };
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).end({ message: "Invalid Admin Token" });
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,8 +12,10 @@ import {
   productListReducer,
 } from "./reducers/productReducers";
 import {
+  userDetailsReducer,
   userRegisterReducer,
   userSigninReducer,
+  userUpdateProfileReducer,
 } from "./reducers/userReducers";
 
 const initialState = {
@@ -42,6 +44,8 @@ const reducer = combineReducers({
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducers,
   orderMineList: orderMineListReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
